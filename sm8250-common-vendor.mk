05ce8da8fb9fa7f911aa213c0ded51e708b9cda8
Michael Bestas <mkbestas@lineageos.org>
2024-06-24 03:10:53 +0300

    sm8250-common: Add symlinks
    Change-Id: Ie68b05e89d130c271b52cba34631447e91d83e73

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -2268,3 +2268,13 @@ PRODUCT_PACKAGES += \
     manifest_oplus_fingerprint \
     vendor.pixelworks.hardware.display.iris-service \
     vendor.pixelworks.hardware.feature.irisfeature-service
+
+PRODUCT_PACKAGES += \
+    libimscamera_jni_libimscamera_jni_symlink64 \
+    libimsmedia_jni_libimsmedia_jni_symlink64 \
+    libEGL_adreno_libEGL_adreno_symlink32 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink32 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink32 \
+    libEGL_adreno_libEGL_adreno_symlink64 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink64 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink64
